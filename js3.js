0817dbd3b1a4023aee9c6634673f646b65c55f25
Olgakorotkova90 <44948334+Olgakorotkova90@users.noreply.github.com>
2019-01-14 10:16:31 +0300

    Update js3.js

--- js3.js
+++ js3.js
@@ -6,10 +6,10 @@ let size=8;
 
 for (y=1; y<=8; y++){
   if (y%2!=0){
-    for(x=' '; x.length<8; x+='#'+' '){}   
+    for(x=''; x.length<8; x+='#'+' '){}   
     }
    else{
-    for(x=' '; x.length<8; x+=' '+'#' ){}   
+    for(x=''; x.length<8; x+=' '+'#' ){}   
   }
 z+=x;
 z+='\n'; 
